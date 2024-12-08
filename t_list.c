b132e9ee4762cdf06c805347767db5d3bf30a156
Cibermatico <149595043+Cibermatico@users.noreply.github.com>
2024-12-08 16:51:44 +0100

    Add files via upload

---
+++ t_list.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_list.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsquarci <lsquarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/08 16:08:36 by lsquarci          #+#    #+#             */
+/*   Updated: 2024/12/08 16:30:25 by lsquarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_list	*lstnew(int value)
+{
+	t_list	*new;
+
+	new = (t_list *) malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->value = value;
+	new->index = -1;
+	new->next = NULL;
+	return (new);
+}
+
+void	ft_lstadd_front(t_list **stack, t_list *new)
+{
+	new->next = *stack;
+	*stack = new;
+}
+
+t_list	*ft_lstlast(t_list *head)
+{
+	t_list	*tmp;
+
+	tmp = head;
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+		if (tmp->next == NULL)
+			return (tmp);
+	}
+	return (tmp);
+}
+
+void	ft_lstadd_back(t_list **stack, t_list *new)
+{
+	t_list	*n;
+
+	if (*stack)
+	{
+		n = ft_lstlast(*stack);
+		n->next = new;
+		new->next = NULL;
+	}
+	else
+	{
+		*stack = new;
+		(*stack)->next = NULL;
+	}
+}
+
+int	lst_size(t_list *head)
+{
+	size_t	i;
+	t_list	*tmp;
+
+	tmp = head;
+	i = 0;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
+
+void	print_list(t_list *head)
+{
+	t_list	*tmp;
+
+	tmp = head;
+	while (tmp != NULL)
+	{
+		ft_putnbr_fd(tmp->value, 1);
+		ft_putendl_fd("", 1);
+		tmp = tmp->next;
+	}
+}
---
+++ utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsquarci <lsquarci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/08 16:09:10 by lsquarci          #+#    #+#             */
+/*   Updated: 2024/12/08 16:25:03 by lsquarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	is_sorted(t_list **stack)
+{
+	t_list	*head;
+
+	head = *stack;
+	while (head && head->next)
+	{
+		if (head->value > head->next->value)
+			return (0);
+		head = head->next;
+	}
+	return (1);
+}
+
+int	get_distance(t_list **stack, int index)
+{
+	t_list	*head;
+	int		distance;
+
+	distance = 0;
+	head = *stack;
+	while (head)
+	{
+		if (head->index == index)
+			break ;
+		distance++;
+		head = head->next;
+	}
+	return (distance);
+}
+
+void	make_top(t_list **stack, int distance)
+{
+	t_list	*head;
+	int		tmp;
+
+	if (distance == 0)
+		return ;
+	head = *stack;
+	tmp = lst_size(head) - distance;
+	if (distance <= (lst_size(head) / 2))
+	{
+		while (distance-- > 0)
+			ra(stack);
+	}
+	else
+	{
+		while (tmp-- > 0)
+			rra(stack);
+	}
+}
+
+void	free_stack(t_list **stack)
+{
+	t_list	*head;
+	t_list	*tmp;
+
+	head = *stack;
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+	free(stack);
+}
